85a659d7a7c8a6fe983c3d46c3445a11019e7482
Yashasvi Sriram Patkuri <patku001@umn.edu>
2020-04-23 22:43:47 -0500

    shifts obstacles and agent to the center while reading scene from file, increases window size

--- src/simulator/Simulator.kt
+++ src/simulator/Simulator.kt
@@ -1,12 +1,14 @@
 package simulator
 
-import extensions.timesAssign
+import extensions.*
 import org.ejml.data.DMatrix2
 import org.ejml.data.DMatrix3
 import processing.core.PApplet
 import java.io.File
 import java.io.FileNotFoundException
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.system.exitProcess
 
 class Simulator(private val applet: PApplet, sceneFilepath: String) {
@@ -46,23 +48,41 @@ class Simulator(private val applet: PApplet, sceneFilepath: String) {
         assert(fileContents.size > 0)
 
         // Every line after 1st one in the file is a line segment
+        val minCorner = DMatrix2(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY)
+        val maxCorner = DMatrix2(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY)
         for (i in 1 until fileContents.size) {
             val lineFeatureTokens = fileContents[i]
             assert(lineFeatureTokens.size == 4)
-            val p1 = DMatrix2(lineFeatureTokens[0].toDouble(), lineFeatureTokens[1].toDouble())
+            minCorner.a1 = min(minCorner.a1, lineFeatureTokens[0].toDouble())
+            minCorner.a1 = min(minCorner.a1, lineFeatureTokens[2].toDouble())
+            minCorner.a2 = min(minCorner.a2, lineFeatureTokens[1].toDouble())
+            minCorner.a2 = min(minCorner.a2, lineFeatureTokens[3].toDouble())
+
+            maxCorner.a1 = max(maxCorner.a1, lineFeatureTokens[0].toDouble())
+            maxCorner.a1 = max(maxCorner.a1, lineFeatureTokens[2].toDouble())
+            maxCorner.a2 = max(maxCorner.a2, lineFeatureTokens[1].toDouble())
+            maxCorner.a2 = max(maxCorner.a2, lineFeatureTokens[3].toDouble())
+        }
+        val center = minCorner + (maxCorner - minCorner) * 0.5
+
+        // Loading lines with scaling and center shifting
+        for (i in 1 until fileContents.size) {
+            val lineTokens = fileContents[i]
+            assert(lineTokens.size == 4)
+            val p1 = DMatrix2(lineTokens[0].toDouble(), lineTokens[1].toDouble()) - center
             p1 *= SCALE
-            val p2 = DMatrix2(lineFeatureTokens[2].toDouble(), lineFeatureTokens[3].toDouble())
+            val p2 = DMatrix2(lineTokens[2].toDouble(), lineTokens[3].toDouble()) - center
             p2 *= SCALE
             lines.add(LineSegmentObstacle(applet, p1, p2))
         }
 
-        // Load robot pose
+        // Load robot pose with scaling and center shifting
         val poseTokens = fileContents[0]
         assert(poseTokens.size == 4)
         // Fork off the main simulation loop
         initialPose = DMatrix3(
-                poseTokens[0].toDouble() * SCALE,
-                poseTokens[1].toDouble() * SCALE,
+                (poseTokens[0].toDouble() - center.a1) * SCALE,
+                (poseTokens[1].toDouble() - center.a2) * SCALE,
                 poseTokens[2].toDouble())
         robot = Robot(
                 applet,
