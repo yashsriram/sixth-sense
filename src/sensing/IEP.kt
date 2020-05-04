7c4c6b0e0361b7cdac943b28d59fac45e26cf74b
Yashasvi Sriram <yash.3997@gmail.com>
2020-05-04 12:08:59 -0500

    fixes partitionBasedOnDiscontinuity() in IEP{}, tunes params, cleans code

--- src/sensing/IEP.kt
+++ src/sensing/IEP.kt
@@ -2,13 +2,14 @@ package sensing
 
 import org.ejml.data.FMatrix2
 import processing.core.PApplet
+import simulator.LaserSensor
 import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 class IEP(private val applet: PApplet) : ObstacleLandmarkExtractor {
     companion object {
-        private const val DISCONTINUITY_THRESHOLD = 60.0
+        private const val DISCONTINUITY_THRESHOLD = 20.0
     }
 
     override fun getName(): String {
@@ -18,24 +19,15 @@ class IEP(private val applet: PApplet) : ObstacleLandmarkExtractor {
     override fun getObservedObstaclesAndLandmarks(inputPoints: List<FMatrix2>, distances: List<Float>): Pair<MutableList<Pair<FMatrix2, FMatrix2>>, MutableList<FMatrix2>> {
         val observedLineSegmentObstacles = mutableListOf<Pair<FMatrix2, FMatrix2>>()
         val observedLandmarks = mutableListOf<FMatrix2>()
-
-        // segment the lines into sections
-        val segments = partitionBasedOnDiscontinuity(inputPoints, distances)
-
-        // find lines in each of the segments
-        for (segment in segments) {
-
-            // Do IEP and add the discovered lines to the list of our lines
-            val linesInSegment = fitLineSegments(segment.toList(), 10F)
-
-            // expose all discovered lines to the visualization
-            for (endPoints in linesInSegment) {
+        val partitions = partitionBasedOnDiscontinuity(inputPoints, distances)
+        for (partition in partitions) {
+            val linesSegmentsInPartition = fitLineSegments(partition.toList(), 10F)
+            for (endPoints in linesSegmentsInPartition) {
                 observedLandmarks.add(endPoints.first)
                 observedLandmarks.add(endPoints.second)
                 observedLineSegmentObstacles.add(endPoints)
             }
         }
-
         return Pair(observedLineSegmentObstacles, observedLandmarks)
     }
 
@@ -43,40 +35,52 @@ class IEP(private val applet: PApplet) : ObstacleLandmarkExtractor {
         val partitions = mutableListOf<MutableList<FMatrix2>>()
         if (points.isNotEmpty()) {
             // Add the first point as
+            var distanceIter = 0
+            while (true) {
+                if (distances[distanceIter] != LaserSensor.INVALID_DISTANCE) {
+                    break
+                }
+                distanceIter++
+            }
             partitions.add(mutableListOf(points[0]))
-
+            distanceIter++
             for (i in 1 until points.size) {
+                while (true) {
+                    if (distances[distanceIter] != LaserSensor.INVALID_DISTANCE) {
+                        break
+                    }
+                    distanceIter++
+                }
                 // Add a new partition every time there is a significant change in distance
-                if (abs(distances[i] - distances[i - 1]) > DISCONTINUITY_THRESHOLD) {
+                if (abs(distances[distanceIter] - distances[distanceIter - 1]) > DISCONTINUITY_THRESHOLD) {
                     partitions.add(mutableListOf())
                 }
                 // Add the point to the latest segment
                 partitions[partitions.lastIndex].add(points[i])
+                distanceIter++
             }
         }
         return partitions
     }
 
     private fun fitLineSegments(points: List<FMatrix2>, epsilon: Float): List<Pair<FMatrix2, FMatrix2>> {
-
         val resultList = mutableListOf<Pair<FMatrix2, FMatrix2>>()
-        var dmax = 0.0F
-        var index = 0
+        var maxDistance = 0.0F
+        var maxIndex = 0
         val end = points.size
-
         if (points.isNotEmpty()) {
-
+            // Assume only one line
             for (i in 1 until end - 1) {
-                val dist = getPerpendicularDistance(points[0], points[end - 1], points[i])
-                if (dist > dmax) {
-                    dmax = dist
-                    index = i
+                val perpendicularDistance = getPerpendicularDistance(points[0], points[end - 1], points[i])
+                if (perpendicularDistance > maxDistance) {
+                    maxDistance = perpendicularDistance
+                    maxIndex = i
                 }
             }
-
-            if (dmax > epsilon) {
-                val resultList1 = fitLineSegments(points.subList(0, index + 1), epsilon)
-                val resultList2 = fitLineSegments(points.subList(index, end), epsilon)
+            // If a point is too far then split the line into two
+            if (maxDistance > epsilon) {
+                val resultList1 = fitLineSegments(points.subList(0, maxIndex + 1), epsilon)
+                val resultList2 = fitLineSegments(points.subList(maxIndex, end), epsilon)
                 resultList.addAll(resultList1)
                 resultList.addAll(resultList2)
             } else {
@@ -91,5 +95,4 @@ class IEP(private val applet: PApplet) : ObstacleLandmarkExtractor {
         val den = sqrt((P2.a2 - P1.a2).toDouble().pow(2.0) + (P2.a1 - P1.a1).toDouble().pow(2.0))
         return (num / den).toFloat()
     }
-
 }
